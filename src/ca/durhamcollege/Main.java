1761011c513b3d60eea41f1af8954871dcbb5812
Nick Sturch-Flint <nick.rulez@live.com>
2020-11-13 14:04:36 -0500

    Validations are Added

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,10 +1,8 @@
 /*
- * Author      @ Nick Sturch-Flint - Kaifkhan Vakil
+ * Author      @ Nick Sturch-Flint - Kaifkhan Vakil (Group 2)
  * Date        @ November 13, 2020
  * Program Name@ Java Lab 1
  */
-
-
 package ca.durhamcollege;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -15,65 +13,71 @@ public class Main
     public static void main(String[] args)
     {
         //Constants
-        int GAMES =   2;
-        int PLAYERS = 3;
-        int MIN_SCORE = 0;
-        int MAX_SCORE = 300;
-
+        final int GAMES =   2;
+        final int PLAYERS = 3;
+        final int MIN_SCORE = 0;
+        final int MAX_SCORE = 300;
 
         //Declarations
         String[] playerNames = {"Nick", "Kaif", "Tom"};
         Scanner keyboard = new Scanner(System.in);
-       // float age = 0.0f;
         boolean dirtyFlag = true;
+        int tempScore = 0;
         double playerAvg = 0;
-        double totalSum = 0;
         int[][] playerGames = new int [PLAYERS][GAMES]; //2Dimensional Array
 
-      //  for (int k = 0; k < PLAYERS; k++)
-     //   {
-    //        System.out.printf("Please enter player %i", k);
-   //     }
 
-        while (dirtyFlag)
-        {
-            try
-            {
-                //USE A FOR LOOP TO STORE THE DATA IN THE ARRAY - GET THE DATA BEFORE USING THE FORLOOP
+                //Nested for loops for taking user input
                 for (int i = 0; i < PLAYERS; i++) //Player counter
                 {
                     System.out.printf("Please enter the scores for %s: \n", playerNames[i]);
 
-
                     for (int j = 0; j < GAMES; j++) //Game counters
                     {
+                        do { //While the input is invalid, keep asking for game scores
+                            try
+                            {
+                                System.out.printf("Please enter the score for game %d: ", j + 1);
+                                tempScore = keyboard.nextInt();
 
-                        System.out.printf("Please enter the score for game %d: ", j + 1);
-                        playerGames[i][j] = keyboard.nextInt();
-
+                                if (tempScore < MIN_SCORE || tempScore > MAX_SCORE) //if the user input is higher or lower than the acceptable inputs
+                                {
+                                    System.out.print("\nYou must enter a valid float greater than 0.00 or lower than 300.00\n");
+                                    keyboard.nextLine();
+                                    dirtyFlag = true;
+                                }
+                                else //if the input is valid
+                                {
+                                    dirtyFlag = false;
+                                    playerGames[i][j] = tempScore;
+                                }
+                            }
+                            catch (InputMismatchException inputMismatchException) //if the input is non-numeric
+                            {
+                                System.out.print("\nYou must enter a valid integer greater than 0.00 or lower than 300.00\n");
+                                keyboard.nextLine();
+                                dirtyFlag = true;
+                            }
+                        } while (dirtyFlag); //using a boolean
                     }
                 }
-            }
-            catch(InputMismatchException inputMismatchException)
+
+            //PRINT THE ARRAY
+            for (int i = 0; i < PLAYERS; i++) //Player counter
             {
 
+                System.out.printf("Printing the scores for %s: \n", playerNames[i]);
+                playerAvg = 0;
+
+                for (int j = 0; j < GAMES; j++) //Game counters
+                {
+                    System.out.printf("Game #   %d: ", j + 1);
+                    System.out.println(playerGames[i][j]);
+                    //take the number (as a string) and cast it as an integer, then store it in playerAvg
+                    playerAvg += playerGames[i][j];
+                }
+                System.out.printf("Average : %.1f\n", (playerAvg / GAMES));
             }
 
-        //PRINT THE ARRAY
-        for (int i = 0; i < PLAYERS; i++) //Player counter
-        {
-
-            System.out.printf("Printing the scores for %s: \n", playerNames[i]);
-            playerAvg = 0;
-
-            for (int j = 0; j < GAMES; j++) //Game counters
-            {
-                System.out.printf("Game #   %d: ", j + 1);
-                System.out.println(playerGames[i][j]);
-                //take the number (as a string) and cast it as an integer, then store it in playerAvg
-                playerAvg += playerGames[i][j];
-            }
-            System.out.printf("Average : %.1f\n", (playerAvg / GAMES));
-        }
     }
 }
